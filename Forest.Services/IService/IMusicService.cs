717baea879c7873a423ec2a12a98f196de24e121
Peck1453 <andy@the-pecks.co.uk>
2018-01-30 12:27:38 +0000

    Added Missing ForestService Files

---
+++ Forest.Services/IService/IMusicService.cs
@@ -0,0 +1,27 @@
+﻿using Forest.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forest.Services.IService
+{
+    public interface IMusicService
+    {
+        IList<Forest.Data.Music_Category> GetMusicCategories();
+        //   IList<Forest.Data.Music_Recording> GetMusicRecordings(string genre);
+        IList<Forest.Data.BEANS.MusicBEAN> GetMusicRecordings(int genre);
+        Forest.Data.Music_Recording GetMusicRecording(int id);
+        Music_Category GetMusicCategory(int id);
+        void EditMusicRecording(Music_Recording recording);
+        void EditMusicGenre(Music_Category category);
+
+        void AddMusicRecording(Music_Recording recording);
+
+        void AddMusicGenre(Music_Category category);
+        void DeleteMusicRecording(Music_Recording recording);
+
+        void DeleteMusicGenre(Music_Category category);
+    }
+}
---
+++ Forest.Services/Service/MusicService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Forest.Data;
+using Forest.Data.DAO;
+using Forest.Data.IDAO;
+
+namespace Forest.Services.Service
+{
+    public class MusicService : Forest.Services.IService.IMusicService
+
+    {
+        private IMusicDAO _musicDAO;
+
+        public MusicService()
+        {
+            _musicDAO = new MusicDAO();
+
+        }
+
+        public IList<Music_Category> GetMusicCategories()
+        {
+            return _musicDAO.GetMusicCategories();
+
+        }
+
+        //public IList<Music_Recording> GetMusicRecordings(string genre)
+        //{
+        //    return _musicDAO.GetMusicRecordings(genre);
+        //}
+        public IList<Forest.Data.BEANS.MusicBEAN> GetMusicRecordings(int genre)
+        {
+            return _musicDAO.GetMusicRecordings(genre);
+        }
+
+        public Music_Recording GetMusicRecording(int id)
+        {
+            return _musicDAO.GetMusicRecording(id);
+        }
+
+        public void EditMusicRecording(Music_Recording recording)
+        {
+            _musicDAO.EditMusicRecording(recording);
+        }
+
+        public Music_Category GetMusicCategory(int id)
+        {
+            return _musicDAO.GetMusicCategory(id);
+
+        }
+
+        public void EditMusicGenre(Music_Category category)
+        {
+            _musicDAO.EditMusicGenre(category);
+        }
+
+
+        public void AddMusicRecording(Music_Recording recording)
+        {
+
+            _musicDAO.AddMusicRecording(recording);
+
+        }
+
+        public void AddMusicGenre(Music_Category category)
+        {
+
+            _musicDAO.AddMusicGenre(category);
+
+        }
+
+        public void DeleteMusicRecording(Music_Recording recording)
+        {
+            _musicDAO.DeleteMusicRecording(recording);
+
+
+        }
+
+        public void DeleteMusicGenre(Music_Category category)
+        {
+
+            _musicDAO.DeleteMusicGenre(category);
+
+        }
+    }
+}
+
